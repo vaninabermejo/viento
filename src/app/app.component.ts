7d6c352384c3f2d2f641aa4b2971dc3b13f2ba93
Vanina Bermejo <vanibermejo@gmail.com>
2016-09-14 13:53:33 -0300

    refactor dashboard html file and navbar html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,29 +11,15 @@ import {FooterNav} from './pages/components/footer-nav';
   directives: [ MaterializeDirective,SideNav ,FooterNav],
   encapsulation: ViewEncapsulation.None,
   template: `
-    <!-- <header>
-    <sideNav></sideNav>
-  </header>
-  <main>
-      <div class="container">
+    <body>
+      <sideNav></sideNav>
+      <main>
         <div class="row">
           <router-outlet></router-outlet>
         </div>
-      </div>
-  </main>
-<footerNav></footerNav> -->
-<body>
-  <sideNav></sideNav>
-<main>
-  <div class="container">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <router-outlet></router-outlet>
-  </div>
-</main>
-<footerNav></footerNav>
-</body>
-
-
+      </main>
+      <footerNav></footerNav>
+    </body>
   `,
   styleUrls: ['./app.component.css']
 })
